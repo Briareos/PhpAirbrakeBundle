b243a92e4a0cec78ea4de1a0a06e6e4cb7a5afca
Milos Colakovic <milos.colakovic@devana.rs>
2015-04-09 10:05:15 +0200

    Remove error_log

--- EventListener/ShutdownListener.php
+++ EventListener/ShutdownListener.php
@@ -56,6 +56,5 @@ class ShutdownListener
         $backtrace = array(array('file' => $error['file'], 'line' => $error['line']));
 
         $this->client->notifyOnError($message, $backtrace);
-        error_log($message.' in: '.$error['file'].':'.$error['line']);
     }
 }
